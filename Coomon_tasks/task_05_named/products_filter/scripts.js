17400901b95ff0cae32eef310d6962e7af300a9d
KoltLevry <157952979+KoltLevry@users.noreply.github.com>
2024-12-13 08:43:50 +0100

    products-filter update

---
+++ Coomon_tasks/task_05_named/products_filter/scripts.js
@@ -0,0 +1,7 @@
+const inputMin = document.querySelector('[name="inputMin"]');
+const inputMax = document.querySelector('[name="inputMax"]');
+const applyButton = document.querySelector('.applyButton');
+const tableList = document.querySelector(".table");
+const saveButton = document.querySelector('.saveButton');
+const resetAllButton = document.querySelector('.resetAllButton');
+
